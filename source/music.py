3545fec889ba7ba8473252daad2d07428de1b42a
Clermont Judicaël <135115381+ClermontJudicael@users.noreply.github.com>
2023-09-04 19:28:38 +0200

    Update music.py

--- source/music.py
+++ source/music.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 # fontion pour les musiques de fond
 
@@ -7,39 +8,37 @@ class music_class:
 
     def play_music1(self):
         self.sound_effect = pygame.mixer.Sound(
-            "C:\\Users\\users\\Desktop\\tic_tac\\music\\background_sound.wav")
+            "./music/background_sound.wav")
         self.sound_effect.set_volume(0.5)
-        self.sound_effect.play(-1)  # -1 plays the music in a loop
+        self.sound_effect.play(-1)
 
     def play_music2(self):
         self.sound_effect = pygame.mixer.Sound(
-            "C:\\Users\\users\\Desktop\\tic_tac\\music\\Nameless_Cat_OST_Dusk.wav")
+            "./music/Nameless_Cat_OST_Dusk.wav")
         self.sound_effect.set_volume(0.5)
-        self.sound_effect.play(-1)  # -1 plays the music in a loop
+        self.sound_effect.play(-1)
 
     def play_music3(self):
         self.sound_effect = pygame.mixer.Sound(
-            "C:\\Users\\users\\Desktop\\tic_tac\\music\\Nameless_Cat_OST_Memory.wav")
+            "./music/Nameless_Cat_OST_Memory.wav")
         self.sound_effect.set_volume(0.5)
-        self.sound_effect.play(-1)  # -1 plays the music in a loop
+        self.sound_effect.play(-1)
 
     def play_music4(self):
         self.sound_effect = pygame.mixer.Sound(
-            "C:\\Users\\users\\Desktop\\tic_tac\\music\\Nameless_Cat_2018_Trailer.wav")
+            "./music/Nameless_Cat_2018_Trailer.wav")
         self.sound_effect.set_volume(0.5)
-        self.sound_effect.play(-1)  # -1 plays the music in a loop
+        self.sound_effect.play(-1)
 
-        # fontion pour les audio X et O
-
-    def play_soundO(self):
+    def play_soundO(self):  # pour O
         self.sound_effect = pygame.mixer.Sound(
-            "C:\\Users\\users\\Desktop\\tic_tac\\music\\O.wav")
+            "./music/O.wav")
         self.sound_effect.set_volume(9)
         self.sound_effect.play()
 
-    def play_soundX(self):
+    def play_soundX(self):  # pour X
         self.sound_effect = pygame.mixer.Sound(
-            "C:\\Users\\users\\Desktop\\tic_tac\\music\\X.wav")
+            "./music/X.wav")
         self.sound_effect.set_volume(9)
         self.sound_effect.play()
 
@@ -47,7 +46,7 @@ class music_class:
 
     def clic(self):
         self.sound_effect = pygame.mixer.Sound(
-            "C:\\Users\\users\\Desktop\\tic_tac\\music\\button_on_clic.wav")
+            "./music/button_on_clic.wav")
         self.sound_effect.set_volume(9)
         self.sound_effect.play()
 
@@ -55,16 +54,14 @@ class music_class:
 
     def win(self):
         self.sound_effect = pygame.mixer.Sound(
-            "C:\\Users\\users\\Desktop\\tic_tac\\music\\Winning_Sound_Effect.wav")
+            "./music/Winning_Sound_Effect.wav")
         self.sound_effect.set_volume(9)
         self.sound_effect.play()
 
-        # fonction pour k'audio quand c'est match nul
+        # fonction pour l'audio quand c'est match nul
 
     def loose(self):
         self.sound_effect = pygame.mixer.Sound(
-            "C:\\Users\\users\\Desktop\\tic_tac\\music\\Fail_sound_effect.wav")
+            "./music/Fail_sound_effect.wav")
         self.sound_effect.set_volume(0.2)
         self.sound_effect.play()
-
-    # def clicX(self):
